fdcdc4779eef6cbf160a852c63754e9d94033d7c
Liz <hyemlee@berkeley.edu>
2016-09-21 15:16:36 -0700

    submitting hw3

--- Assignment/HW3-JS_ToDo_List/_js/main.js
+++ Assignment/HW3-JS_ToDo_List/_js/main.js
@@ -1,13 +1,34 @@
-$(document).ready(
+$(document).ready(function() {
+    console.log("Ready!");
     $("#new-item").on('click', function() {
-        // once the document loads, create new item with this function
-    })
-);
+        // once the document loads, 
+        // create new item with this function
+        var inputValue = $("input").val();
+        $("#to-do").prepend('<li><button> Finished? </button>'+inputValue+'</li>');
+    });
 
-$("#list_todo").on('click', "button", function() {
-        // move from list_todo container to list_completed container
+
+    $("#list_todo").on('click', "button", function() {
+        // move from list_todo container to 
+        // list_completed container
+        $(this).html("Not finished?");
+        var completed = $(this).parent();
+        completed.css("color","Grey");
+        completed.css("font-style","italic");
+        $("#complete").prepend(completed);
+
+    });
+
+    $("#list_completed").on('click', "button", function() {
+        // move back from list_completed container to 
+        // list_todo container
+        $(this).html("Finished?");
+        var stilldo = $(this).parent();
+        stilldo.css("color", "black");
+        stilldo.css("font-style", "normal");
+        $("#to-do").prepend(stilldo);
+
+    });
 });
 
-$("#list_completed").on('click', "button", function() {
-        // move back from list_completed container to list_todo container
-});
+
